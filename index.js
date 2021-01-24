0ecd8531cd7e77f9dba4456ef9016fc4538987eb
Rawle Springer <rspringerdeveloper@gmail.com>
2021-01-23 21:37:55 -0500

    added delete functionality

--- index.js
+++ index.js
@@ -17,11 +17,23 @@ let todos = [
   },
 ];
 
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+// const $todos = document.querySelector(".main");
+
 //DISPLAY TODOS
 //background-color: #FFFFF4;
 
 const createTodoElement = (todo) => {
   const $todoContainer = document.createElement("div");
+  $todoContainer.setAttribute("data-id", todo.id.toString());
+  $todoContainer.classList.add("todo-container");
   $todoContainer.style.cssText = `
       background-color: #FFF;
       height: 60px;
@@ -60,34 +72,60 @@ const createTodoElement = (todo) => {
   $todoTextContainer.append($todoText);
   $todoTextContainer.append($todoDueDate);
 
+  const $todoDelete = document.createElement("img");
+  $todoDelete.classList.add("delete");
+  $todoDelete.setAttribute("data-id", todo.id.toString());
+  $todoDelete.src = "./icon-trash.svg";
+
+  $todoDelete.addEventListener("click", (e) => {
+    deleteTodo(e.target);
+    // console.log(e.target);
+    // displayTodos();
+  });
+
+  $todoDelete.style.cssText = `
+  height: 20px;
+  width: 20px;
+`;
+
   const $todoFavorite = document.createElement("img");
-  $todoFavorite.src = "./star.svg";
+  $todoFavorite.classList.add("favorite");
+  $todoFavorite.src = "./icon-target.svg";
 
   $todoFavorite.style.cssText = `
     height: 20px;
     width: 20px;
+    margin-left: 10px;
   `;
 
   $todoContainer.append($todoCircle);
   $todoContainer.append($todoTextContainer);
+  $todoContainer.append($todoDelete);
   $todoContainer.append($todoFavorite);
 
   return $todoContainer;
 };
 
-const $todos = document.querySelector(".main");
+const displayTodos = () => {
+  const $todos = document.querySelector(".main");
+  $todos.innerHTML = "";
+  if (todos.length == 0) {
+    return;
+  }
 
-todos.forEach((todo) => {
-  let $todoItem = createTodoElement(todo);
-  $todos.append($todoItem);
-});
+  todos.forEach((todo) => {
+    let $todoItem = createTodoElement(todo);
+
+    $todos.append($todoItem);
+  });
+};
 
 //ADD TODO
 
 const addTodo = () => {
   const $todoTitle = document.querySelector("textarea");
   const $todoDueDate = document.querySelector(".dueDate");
-  const id = todos.length;
+  const id = uuidv4();
   const title = $todoTitle.value;
   const status = false;
   const createDate = new Date().toDateString();
@@ -102,13 +140,14 @@ const addTodo = () => {
   };
 
   todos.push(todo);
-  const $todoItem = createTodoElement(todo);
-  $todos.append($todoItem);
+  displayTodos();
 };
+
 const $modal = document.querySelector(".modal");
 const $addTodoIcon = document.querySelector(".add");
 const $newTodo = document.querySelector(".newTodo");
 const $exitTodoIcon = document.querySelector(".exit");
+
 const $addTodoSubmitButton = document.querySelector(".add-todo-button");
 
 $addTodoIcon.addEventListener("click", () => {
@@ -129,6 +168,17 @@ $addTodoSubmitButton.addEventListener("click", () => {
 });
 
 //DELETE TODO
+const deleteTodo = (todoEl) => {
+  const parent = todoEl.parentNode;
+
+  const newTodoList = todos.filter((todo) => {
+    return parseInt(todoEl.dataset.id) !== todo.id;
+  });
+
+  todos = newTodoList;
+  console.log(todos);
+  parent.remove();
+};
 
 //EDIT TODO
 
@@ -143,3 +193,15 @@ $addTodoSubmitButton.addEventListener("click", () => {
 //FILTER
 
 //EVENT LISTENERS
+
+displayTodos();
+
+const $deleteTodoIcon = document.querySelectorAll(".delete");
+// const $favoriteTodoIcon = document.querySelectorAll(".favorite");
+
+// $deleteTodoIcon.forEach((todo) => {
+//   todo.addEventListener("click", (e) => {
+//     deleteTodo(e.target);
+//     displayTodos();
+//   });
+// });
